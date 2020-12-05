7240defeed11e7a5140cb0e2108426c8448db2ef
Johan Basson <johan@datafox.co.za>
2020-12-05 13:55:46 +0200

    Added xmonad applications

--- setup.sh
+++ setup.sh
@@ -10,10 +10,10 @@ sudo pacman -Syu
 sudo pacman -S aspell-en libmythes mythes-en languagetool yay --noconfirm
 
 ## Packages from arch
-sudo pacman -S plank intel-ucode chromium code alacritty rofi zsh zsh-syntax-highlighting neovim neovim-qt exa nvm vlc flameshot meld ttf-fira-code jdk8-openjdk openjdk8-doc openjdk8-src java8-openjfx maven gradle bleachbit docker docker-compose emacs libreoffice-still clojure ripgrep leiningen --noconfirm
+sudo pacman -S plank intel-ucode chromium code alacritty rofi zsh zsh-syntax-highlighting neovim neovim-qt exa nvm vlc flameshot meld ttf-fira-code jdk8-openjdk openjdk8-doc openjdk8-src java8-openjfx maven gradle bleachbit docker docker-compose emacs libreoffice-still clojure ripgrep leiningen xmonad xmona-contrib xmobar nitrogen volume-icon stalonetray --noconfirm
 
 # Packages from aur
-yay -S ttf-ms-fonts google-chrome skypeforlinux-stable-bin slack-desktop telegram-desktop yadm brave lf bitwarden-bin clj-kondo-bin --noconfirm
+yay -S ttf-ms-fonts google-chrome skypeforlinux-stable-bin slack-desktop telegram-desktop yadm brave lf bitwarden-bin clj-kondo-bin picoma nerd-fonts-jetbrains-mono xsettingsd --noconfirm
 
 # Snap packages
 snap install spotify
